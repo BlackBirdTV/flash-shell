62bcdf93adf0a7ec3aae543e9ef9ef493fc5504f
BlackBird <93947677+BlackBirdTV@users.noreply.github.com>
2022-09-06 20:03:27 +0200

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 cwd=$(pwd)
 cd
-curl "http://github.com/BlackBirdTV/flash-shell/releases/latest/download/flash-shell" --output flash-shell
-chmod 777 flash-shell
+curl -L -O "https://github.com/BlackBirdTV/flash-shell/releases/latest/download/flash-shell" --output flash-shell
+chmod 555 flash-shell
 sudo cp flash-shell /usr/bin/flash-shell
 rm flash-shell
 cd $cwd
