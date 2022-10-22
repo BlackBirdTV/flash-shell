7541c0be973b02b3e3556bcbd9a7369058be348e
BlackBirdTV <chronicalcoding@gmail.com>
2022-10-22 11:57:29 +0200

    Fixed setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
+#!/bin/bash
+
 cwd=$(pwd)
 cd ~
-curl -L -O "https://github.com/BlackBirdTV/flash-shell/releases/latest/download/flash-shell" --output flash-shell
-if [ ! -d /usr/bin/flash-shell ]
-then
-    mkdir /usr/bin/flash-shell
-fi
+sudo curl "https://github.com/BlackBirdTV/flash-shell/releases/latest/download/flash-shell" > flash-shell
+sudo rm -rf /usr/bin/flash-shell
+sudo mkdir /usr/bin/flash-shell
 sudo cp flash-shell /usr/bin/flash-shell
 sudo chmod 777 /usr/bin/flash-shell
 cd $cwd
