68543ef6cd09f5a35614760f9e6e0a20a48a1cad
BlackBird <93947677+BlackBirdTV@users.noreply.github.com>
2022-10-22 11:26:01 +0200

    Added `-p` flag to `mkdir` statement

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 cwd=$(pwd)
 cd ~
 curl -L -O "https://github.com/BlackBirdTV/flash-shell/releases/latest/download/flash-shell" --output flash-shell
-sudo mkdir /usr/bin/flash-shell
+sudo mkdir -p /usr/bin/flash-shell
 sudo cp flash-shell /usr/bin/flash-shell
 sudo chmod 777 /usr/bin/flash-shell
 cd $cwd
