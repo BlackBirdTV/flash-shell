23ea40b071d96bcf3db6efd7ebcc2c904b0cf747
BlackBird <93947677+BlackBirdTV@users.noreply.github.com>
2022-10-22 11:29:38 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 cwd=$(pwd)
 cd ~
 curl -L -O "https://github.com/BlackBirdTV/flash-shell/releases/latest/download/flash-shell" --output flash-shell
-if [ -d /usr/bin/flash-shell ]
+if [ ! -d /usr/bin/flash-shell ]
 then
     mkdir /usr/bin/flash-shell
 fi
